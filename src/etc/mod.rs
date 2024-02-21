bdc4bedb5603bf95f614d30476f55a644b0b2371
puffy_with_eyes <klimov-danya.04@mail.ru>
2024-02-21 05:00:30 +0300

    IM RUSSIANNNNNNN

--- src/etc/mod.rs
+++ src/etc/mod.rs
@@ -3,5 +3,5 @@ use std::convert::TryInto;
 
 pub fn vec_to_arr<T, const N: usize>(v: Vec<T>) -> [T; N] {
     v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
+        .unwrap_or_else(|v: Vec<T>| panic!("Ожидался вектор с длинной {}, но он был динной {}", N, v.len()))
 }
