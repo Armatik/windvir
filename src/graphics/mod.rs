a42176247385a22b656f247ae90edf930b8fb8f1
puffy_with_eyes <klimov-danya.04@mail.ru>
2024-02-22 04:46:06 +0300

    New map viewing modes have been added, although they are still unfinished. The code was also refactored

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -4,6 +4,48 @@ pub struct Vertex {
 }
 
 
+pub const VERTEX_SHADER_PATH: &str = "src/graphics/vertex_shader.vert";
+pub const COLOR_SHADER_PATH: &str = "src/graphics/color_shader.vert";
+pub const OFFSET_X: f32 = -45.395;
+pub const OFFSET_Y: f32 = -52.293;
+pub const BACKGROUND_R: f32 = 0.8;
+pub const BACKGROUND_G: f32 = 0.98;
+pub const BACKGROUND_B: f32 = 0.988;
+pub const NOT_TRANSPARENT: f32 = 1.;
+pub const ASPECT_RATIO_WIDTH: f32 = 16.;
+pub const ASPECT_RATIO_HEIGHT: f32 = 9.;
+
+
+pub enum TransformAction {
+    Increase,
+    Reduce,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    RotateRight,
+    RotateLeft,
+}
+
+
+pub enum DisplayType {
+    Triangles,
+    TrianglesLines,
+    Lines,
+}
+
+
+impl DisplayType {
+    pub fn switch(&mut self) {
+        *self = match self {
+            Self::Triangles => Self::TrianglesLines,
+            Self::TrianglesLines => Self::Lines,
+            Self::Lines => Self::Triangles,
+        }
+    }
+}
+
+
 pub fn get_triangle_indices(buildings: &Vec<Vec<Vec<f64>>>) -> Vec<u16> {
     let mut indices = Vec::<u16>::new();
     let mut index = 0;
@@ -34,6 +76,26 @@ pub fn get_triangle_indices(buildings: &Vec<Vec<Vec<f64>>>) -> Vec<u16> {
 
             index += 1;
         }
+    }
+
+    indices
+}
+
+
+pub fn get_line_indices(buildings: &Vec<Vec<Vec<f64>>>) -> Vec<u16> {
+    let mut indices = Vec::<u16>::new();
+    let mut index = 0;
+
+    for building in buildings {
+        let init_index = index;
+
+        for _ in building {
+            indices.append(&mut vec![index, index + 1]);
+            index += 1;
+        }
+
+        indices.append(&mut vec![index, init_index]);
+        index += 1;
     }
 
     indices
--- src/main.rs
+++ src/main.rs
@@ -16,30 +16,6 @@ use graphics::Vertex;
 use std::fs;
 
 
-const VERTEX_SHADER_PATH: &str = "src/graphics/vertex_shader.vert";
-const COLOR_SHADER_PATH: &str = "src/graphics/color_shader.vert";
-const OFFSET_X: f32 = -45.395;
-const OFFSET_Y: f32 = -52.293;
-const BACKGROUND_R: f32 = 0.8;
-const BACKGROUND_G: f32 = 0.98;
-const BACKGROUND_B: f32 = 0.988;
-const NOT_TRANSPARENT: f32 = 1.;
-const ASPECT_RATIO_WIDTH: f32 = 16.;
-const ASPECT_RATIO_HEIGHT: f32 = 9.;
-
-
-enum TransformAction {
-    Increase,
-    Reduce,
-    MoveUp,
-    MoveDown,
-    MoveLeft,
-    MoveRight,
-    RotateRight,
-    RotateLeft,
-}
-
-
 struct App {
     p: json::Persistent,
     scale: f32,
@@ -47,6 +23,7 @@ struct App {
     offset_y: f32,
     theta: f32,
     transform_matrix: [[f32; 4]; 4],
+    display_type: graphics::DisplayType,
 }
 
 
@@ -58,36 +35,37 @@ impl App {
         Self {
             p: _p,
             scale: SCALE,
-            offset_x: OFFSET_X,
-            offset_y: OFFSET_Y,
+            offset_x: graphics::OFFSET_X,
+            offset_y: graphics::OFFSET_Y,
             theta: THETA,
             transform_matrix: [
-                [f32::cos(THETA) * SCALE * ASPECT_RATIO_HEIGHT / ASPECT_RATIO_WIDTH, -f32::sin(THETA), 0., 0.],
+                [f32::cos(THETA) * SCALE * graphics::ASPECT_RATIO_HEIGHT / graphics::ASPECT_RATIO_WIDTH, -f32::sin(THETA), 0., 0.],
                 [f32::sin(THETA), f32::cos(THETA) * SCALE, -f32::sin(THETA), 0.],
                 [0., f32::sin(THETA), f32::cos(THETA), 0.],
                 [0., 0., 0., 1.],
-            ]
+            ],
+            display_type: graphics::DisplayType::Triangles,
         }
     }
 
-    fn transform_map(&mut self, action: TransformAction) {
+    fn transform_map(&mut self, action: graphics::TransformAction) {
         const OFFSET_SCALE: f32 = 7.;
-        const OFFSET_WIDTH: f32 = 0.0001;
-        const OFFSET_HEIGHT: f32 = 0.00007;
+        const OFFSET_X: f32 = 0.0001;
+        const OFFSET_Y: f32 = 0.00007;
         const OFFSET_THETA: f32 = 0.04;
         let transform = |mat: &mut [[f32; 4]; 4], theta: f32, scale: f32| {
-            mat[0][0] = f32::cos(theta) * scale * ASPECT_RATIO_HEIGHT / ASPECT_RATIO_WIDTH;
+            mat[0][0] = f32::cos(theta) * scale * graphics::ASPECT_RATIO_HEIGHT / graphics::ASPECT_RATIO_WIDTH;
             mat[0][1] = -f32::sin(theta) * scale;
             mat[1][0] = f32::sin(theta) * scale;
             mat[1][1] = f32::cos(theta) * scale;
         };
         
         match action {
-            TransformAction::Increase => {
+            graphics::TransformAction::Increase => {
                 self.scale += OFFSET_SCALE;
                 transform(&mut self.transform_matrix, self.theta, self.scale);
             },
-            TransformAction::Reduce => {
+            graphics::TransformAction::Reduce => {
                 if self.scale - OFFSET_SCALE < 0. {
                     return;
                 }
@@ -95,15 +73,15 @@ impl App {
                 self.scale -= OFFSET_SCALE;
                 transform(&mut self.transform_matrix, self.theta, self.scale);
             },
-            TransformAction::MoveUp => self.offset_y -= OFFSET_HEIGHT,
-            TransformAction::MoveDown => self.offset_y += OFFSET_HEIGHT,
-            TransformAction::MoveLeft => self.offset_x += OFFSET_WIDTH,
-            TransformAction::MoveRight => self.offset_x -= OFFSET_WIDTH,
-            TransformAction::RotateLeft => {
+            graphics::TransformAction::MoveUp => self.offset_y -= OFFSET_Y,
+            graphics::TransformAction::MoveDown => self.offset_y += OFFSET_Y,
+            graphics::TransformAction::MoveLeft => self.offset_x += OFFSET_X,
+            graphics::TransformAction::MoveRight => self.offset_x -= OFFSET_X,
+            graphics::TransformAction::RotateLeft => {
                 self.theta += OFFSET_THETA;
                 transform(&mut self.transform_matrix, self.theta, self.scale);
             },
-            TransformAction::RotateRight => {
+            graphics::TransformAction::RotateRight => {
                 self.theta -= OFFSET_THETA;
                 transform(&mut self.transform_matrix, self.theta, self.scale);
             },
@@ -114,15 +92,53 @@ impl App {
         &self,
         display: &Display<WindowSurface>,
         positions: &VertexBuffer<Vertex>,
-        indices: &IndexBuffer<u16>,
+        indices: (&IndexBuffer<u16>, &IndexBuffer<u16>),
         program: &glium::Program,
-        draw_parameters: &glium::DrawParameters<'_>,
     ) {
         let uniforms = uniform! { matrix: self.transform_matrix, x_off: self.offset_x, y_off: self.offset_y };
         let mut target = display.draw();
-        target.clear_color(BACKGROUND_R, BACKGROUND_G, BACKGROUND_B, NOT_TRANSPARENT);
-        target.draw(&*positions, &*indices, &program, &uniforms, &draw_parameters)
-            .expect("Ошибка! Не удалось отрисовать кадр!");
+        target.clear_color(
+            graphics::BACKGROUND_R,
+            graphics::BACKGROUND_G,
+            graphics::BACKGROUND_B,
+            graphics::NOT_TRANSPARENT
+        );
+
+        match self.display_type {
+            graphics::DisplayType::Triangles => {
+                let params = glium::DrawParameters {
+                    polygon_mode: glium::draw_parameters::PolygonMode::Fill,
+                    multisampling: true,
+                    dithering: true,
+                    smooth: Some(glium::draw_parameters::Smooth::Nicest),
+                    ..Default::default()
+                };
+                target.draw(&*positions, &*indices.0, &program, &uniforms, &params)
+                    .expect("Ошибка! Не удалось отрисовать кадр!");
+            },
+            graphics::DisplayType::TrianglesLines => {
+                let params = glium::DrawParameters {
+                    polygon_mode: glium::draw_parameters::PolygonMode::Line,
+                    multisampling: true,
+                    dithering: true,
+                    smooth: Some(glium::draw_parameters::Smooth::Nicest),
+                    ..Default::default()
+                };
+                target.draw(&*positions, &*indices.0, &program, &uniforms, &params)
+                    .expect("Ошибка! Не удалось отрисовать кадр!");
+            },
+            graphics::DisplayType::Lines => {
+                let params = glium::DrawParameters {
+                    multisampling: true,
+                    dithering: true,
+                    smooth: Some(glium::draw_parameters::Smooth::Nicest),
+                    ..Default::default()
+                };
+                target.draw(&*positions, &*indices.1, &program, &uniforms, &params)
+                    .expect("Ошибка! Не удалось отрисовать кадр!");
+            },
+        }
+
         target.finish()
             .expect("Ошибка! Не удалось закончить отрисовку кадра!");
     }
@@ -130,11 +146,13 @@ impl App {
     pub fn start_app(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let mut buildings = Vec::<Vec<Vec<f64>>>::with_capacity(self.p.features.len());
 
-        for build in &self.p.features {
-            buildings.push(build.geometry.coordinates[0][0].clone());
+        for building in &self.p.features {
+            buildings.push(building.geometry.coordinates[0][0].clone());
         }
         
-        let indices = graphics::get_triangle_indices(&buildings);
+        let indices_triangle = graphics::get_triangle_indices(&buildings);
+        let indices_line = graphics::get_line_indices(&buildings);
+        println!("{:?}", indices_line);
         let event_loop = winit::event_loop::EventLoopBuilder::new()
             .build()?;
         let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
@@ -150,23 +168,25 @@ impl App {
         }
 
         let positions = glium::VertexBuffer::new(&display, &shape)?;
-        let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &indices)?;        
-        let vertex_shader_src = fs::read_to_string(VERTEX_SHADER_PATH)?;
-        let color_shader_src = fs::read_to_string(COLOR_SHADER_PATH)?;
+        let indices_triangle = glium::IndexBuffer::new(
+            &display,
+            glium::index::PrimitiveType::TrianglesList,
+            &indices_triangle,
+        )?;
+        let indices_line = glium::IndexBuffer::new(
+            &display,
+            glium::index::PrimitiveType::LineLoop,
+            &indices_line,
+        )?;
+        let vertex_shader_src = fs::read_to_string(graphics::VERTEX_SHADER_PATH)?;
+        let color_shader_src = fs::read_to_string(graphics::COLOR_SHADER_PATH)?;
         let program = glium::Program::from_source(
             &display,
             &vertex_shader_src,
             &color_shader_src,
             None,
         )?;
-        let params = glium::DrawParameters {
-            polygon_mode: glium::draw_parameters::PolygonMode::Fill,
-            multisampling: true,
-            dithering: true,
-            smooth: Some(glium::draw_parameters::Smooth::Nicest),
-            ..Default::default()
-        };
-        self.render_frame(&display, &positions, &indices, &program, &params);
+        self.render_frame(&display, &positions, (&indices_triangle, &indices_line), &program);
 
         event_loop.run(move |ev, window_target| {
             match ev {
@@ -175,22 +195,20 @@ impl App {
                         window_target.exit();
                     },
                     winit::event::WindowEvent::Resized(window_size) => {
-                        self.render_frame(&display, &positions, &indices, &program, &params);
+                        self.render_frame(&display, &positions, (&indices_triangle, &indices_line), &program);
                         display.resize(window_size.into());
                     },
                     winit::event::WindowEvent::Focused(_) => self.render_frame(
                         &display,
                         &positions,
-                        &indices,
+                        (&indices_triangle, &indices_line),
                         &program,
-                        &params,
                     ),
                     winit::event::WindowEvent::Moved(_) => self.render_frame(
                         &display,
                         &positions,
-                        &indices,
+                        (&indices_triangle, &indices_line),
                         &program,
-                        &params,
                     ),
                     winit::event::WindowEvent::KeyboardInput { event, is_synthetic, .. } => {
                         if is_synthetic {
@@ -200,18 +218,21 @@ impl App {
                         match event.physical_key {
                             winit::keyboard::PhysicalKey::Code(key) => {
                                 match key {
-                                    winit::keyboard::KeyCode::KeyZ => self.transform_map(TransformAction::Increase),
-                                    winit::keyboard::KeyCode::KeyX => self.transform_map(TransformAction::Reduce),
-                                    winit::keyboard::KeyCode::KeyW => self.transform_map(TransformAction::MoveUp),
-                                    winit::keyboard::KeyCode::KeyS => self.transform_map(TransformAction::MoveDown),
-                                    winit::keyboard::KeyCode::KeyA => self.transform_map(TransformAction::MoveLeft),
-                                    winit::keyboard::KeyCode::KeyD => self.transform_map(TransformAction::MoveRight),
-                                    winit::keyboard::KeyCode::KeyQ => self.transform_map(TransformAction::RotateLeft),
-                                    winit::keyboard::KeyCode::KeyE => self.transform_map(TransformAction::RotateRight),
+                                    winit::keyboard::KeyCode::KeyZ => self.transform_map(graphics::TransformAction::Increase),
+                                    winit::keyboard::KeyCode::KeyX => self.transform_map(graphics::TransformAction::Reduce),
+                                    winit::keyboard::KeyCode::KeyW => self.transform_map(graphics::TransformAction::MoveUp),
+                                    winit::keyboard::KeyCode::KeyS => self.transform_map(graphics::TransformAction::MoveDown),
+                                    winit::keyboard::KeyCode::KeyA => self.transform_map(graphics::TransformAction::MoveLeft),
+                                    winit::keyboard::KeyCode::KeyD => self.transform_map(graphics::TransformAction::MoveRight),
+                                    winit::keyboard::KeyCode::KeyQ => self.transform_map(graphics::TransformAction::RotateLeft),
+                                    winit::keyboard::KeyCode::KeyE => self.transform_map(graphics::TransformAction::RotateRight),
+                                    winit::keyboard::KeyCode::KeyC => if event.state == winit::event::ElementState::Released {
+                                        self.display_type.switch();
+                                    },
                                     _ => return,
                                 }
 
-                                self.render_frame(&display, &positions, &indices, &program, &params);
+                                self.render_frame(&display, &positions, (&indices_triangle, &indices_line), &program);
                             },
                             winit::keyboard::PhysicalKey::Unidentified(_) => return,
                         }
