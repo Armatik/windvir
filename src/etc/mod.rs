f49a3b7beef01de64a7f6e01bf7feb28f63e3fe8
puffy_with_eyes <klimov-danya.04@mail.ru>
2024-03-29 03:45:59 +0300

    Move mod in file

--- src/etc/mod.rs
+++
@@ -1,4 +0,0 @@
-pub fn vec_to_arr<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Ожидался вектор с длинной {}, но он был динной {}", N, v.len()))
-}
