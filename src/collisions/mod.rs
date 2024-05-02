fbbc350e1d3bf876a7ebcb1c27b8c6780d0e957e
hating <peaceful0strangeac@gmail.com>
2024-05-02 19:53:24 +0300

    Added check dor point inside building

--- src/collisions/mod.rs
+++ src/collisions/mod.rs
@@ -17,6 +17,28 @@ fn test_vector_intersection(first: &Vector, second: &Vector) -> bool {
     s1*s1 < s1*cross_product && s2*s2 < s2*cross_product
 }
 
+fn test_if_positive_infinity_vector_crosses_side(point: &PositionVector, side: &Vector) -> isize {
+    if side.offset.y == 0. { return 0isize; }
+    let s = (point.y - side.position.y)/side.offset.y;
+    if 
+    s*s < s
+    &&
+    s*side.offset.x + side.position.x > point.x
+    {
+        if side.offset.y > 0.0 { return 1isize; }
+        else { return -1isize; }
+    }
+    0isize
+
+}
+
+fn test_if_point_inside_building(point: &PositionVector, building: &Building) -> bool {
+    let mut count = 0isize;
+    for side in building.sides.iter() {
+        count += test_if_positive_infinity_vector_crosses_side(point, side);
+    }
+    count == 0isize
+}
 
 #[inline]
 fn get_segment_division_parameter(first: &Vector, second: &Vector) -> f32 {
@@ -40,8 +62,8 @@ fn test_bounding_box_intersection(first: &Building, second: &Building) -> bool {
 }
 
 // Не оптимизированно
-fn _test_bounding_box_intersection(first: &Building, second: &Building) -> bool {
-    if _check_bounding_box_intersection(first, second) {
+fn _test_building_intersection(first: &Building, second: &Building) -> bool {
+    if _test_bounding_box_intersection(first, second) {
         for first_building_side in first.sides.iter() {
             for second_building_side in second.sides.iter() {
                 if test_vector_intersection(first_building_side, second_building_side) {
@@ -58,7 +80,7 @@ fn _test_bounding_box_intersection(first: &Building, second: &Building) -> bool 
 fn _optimize_map(sorted_map: &Vec<Building>) -> () {
     for i in 0usize..sorted_map.len().saturating_sub(1usize) {
         for n in i + 1usize..sorted_map.len() {
-            if _check_building_intersection(&sorted_map[i], &sorted_map[n]) {
+            if _test_building_intersection(&sorted_map[i], &sorted_map[n]) {
                 todo!(); // Функция которая определит что делать с пересечениями
             }
         }
