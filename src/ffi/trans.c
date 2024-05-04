ad43bf177acbdd33cd6f3b5181716dbb48eff326
yatafan <nadgob358@gmail.com>
2024-05-04 18:34:00 +0300

    Алгоритм объединения зданий

--- src/ffi/trans.c
+++ src/ffi/trans.c
@@ -4,23 +4,17 @@
 #include <stdio.h>
 
 bool
-is_lefter(PointC *a, PointC *b, PointC *main){
+is_lefter(const PointC *a, const PointC *b, const PointC *main){
 	return ( ((a->y - main->y) / (a->x - main->x) > (b->y - main->y) / (b->x - main->x)) );
 }
 
 int
-compare_buildings(const void *b1, const void *b2){
-	float x1 = ((BuildingC*)b1)->startPoint.x;
-	float x2 = ((BuildingC*)b2)->startPoint.x;
+compare_buildings(const void * b1, const void * b2){
+	double x1 = (*(BuildingC*)b1).startPoint.x;
+	double x2 = (*(BuildingC*)b2).startPoint.x;
 
-	if(x1 < x2){
-		return -1;
-	}
-	else{
-		return 1;
-	}
-
-	return 0;
+	printf("%f %f\n", x1, x2);
+	return (int)(x1 - x2);
 }
 
 void
@@ -37,7 +31,7 @@ swap_points(PointC *a, PointC *b){
 void
 grahams_algorithm(BuildingC *building){
 	PointC leftmost_point; //Самая левая точка здания
-	PointC* points; // Массив точек(вершин) здания
+	register PointC* points; // Массив точек(вершин) здания
 	
 	points = malloc(building->lenVertex * sizeof(PointC));
 
@@ -85,7 +79,7 @@ grahams_algorithm(BuildingC *building){
 	}
 	
 	uint64_t result_size = 2;
-	PointC *result_points = malloc(result_size * sizeof(PointC)); // Точки, составляющие выпуклую оболочку
+	register PointC *result_points = malloc(result_size * sizeof(PointC)); // Точки, составляющие выпуклую оболочку
 
 	result_points[0] = points[0];
 	result_points[1] = points[1];
@@ -136,6 +130,31 @@ grahams_algorithm(BuildingC *building){
 	free(result_points);
 }
 
+BuildingC*
+merge_buildings(BuildingsVec *buildings_vec){
+	BuildingC *result_building = malloc(sizeof(BuildingC));
+	uint64_t points_len = 0; // Количество точек
+
+	for(uint64_t i = 0; i < buildings_vec->lenBuildings; i++){
+		points_len += buildings_vec->buildings[i].lenVertex;
+	}
+
+	result_building->sides = malloc(points_len * sizeof(VectorC));
+	result_building->lenVertex = points_len;
+
+	// index это итератор по массиву sides
+	for(uint64_t i = 0, index = 0; i < buildings_vec->lenBuildings; i++){
+		for(uint64_t j = 0; j < buildings_vec->buildings[i].lenVertex; j++){
+			result_building->sides[index].position = buildings_vec->buildings[i].sides[j].position;
+			index++;
+		}
+	}
+
+	grahams_algorithm(result_building);
+
+	return result_building;
+}
+
 BuildingsVec
 changeVertex(BuildingsVec data)
 {
@@ -145,8 +164,45 @@ changeVertex(BuildingsVec data)
 	}
 
 	// Сортировка зданий по левой границе
-	qsort(data.buildings, data.lenBuildings, sizeof(BuildingC), compare_buildings);
+	//qsort(data.buildings, data.lenBuildings, sizeof(BuildingC), compare_buildings);
+	
+	for(uint64_t i = 0; i < data.lenBuildings; ++i){
+		for(uint64_t j = 0; j < data.lenBuildings - 1; ++j){
+			if(data.buildings[j].startPoint.x > data.buildings[j+1].startPoint.x){
+				BuildingC tmp = data.buildings[j];
+				data.buildings[j] = data.buildings[j + 1];
+				data.buildings[j + 1] = tmp;
+			}
+		}
+	}
+	
+	/* ---------- Проверка объединения зданий ---------- */
+/*
+	BuildingsVec vec;
+	uint64_t n = 5;
+	vec.lenBuildings = n;
+	vec.buildings = malloc(vec.lenBuildings * sizeof(BuildingC));
 
+	for(uint64_t i = 0; i < vec.lenBuildings; i++){
+		vec.buildings[i] = data.buildings[i];
+	}
+
+	BuildingC* result = merge_buildings(&vec);
+
+	free(vec.buildings);
+
+	BuildingsVec new_data;
+	new_data.lenBuildings = data.lenBuildings - n + 1;
+	new_data.buildings = malloc(new_data.lenBuildings * sizeof(BuildingC));
+
+	new_data.buildings[0] = *result;
+
+	for(uint64_t i = 1; i < new_data.lenBuildings; i++){
+		new_data.buildings[i] = data.buildings[i+n-1];
+	}
+
+	return new_data;
+*/
 	return data;
 }
 
@@ -157,7 +213,7 @@ freeBuildings(BuildingsVec data)
 {
 	for (uint64_t i = 0; i < data.lenBuildings; ++i)
 	{
-		free((void *)data.buildings[i].sides);
+		free(data.buildings[i].sides);
 	}
 
 	free(data.buildings);
