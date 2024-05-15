2d702ca37fc1ec301d0bd1f546be3f924afab75e
IronDust <irondustmail@gmail.com>
2024-05-15 20:32:23 +0300

    Started making non-convex hull algorythm

--- include/trans.h
+++ include/trans.h
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,16 +43,22 @@ BuildingsVec
 changeVertex(BuildingsVec data);
 
 bool
-is_lefter(PointC* a, PointC* b, PointC* main); // Говорит находится ли точка b левее точки a относительно точки main
+is_lefter(const PointC* a, const PointC* b, const PointC* main); // Говорит находится ли точка b левее точки a относительно точки main
 
 int
-compare_buildings(const void *b1, const void *b2); // Для сортировки зданий по левой границе
+compare_buildings(const void * b1, const void * b2); // Для сортировки зданий по левой границе
 
 void
 swap_points(PointC* a, PointC* b); // Меняет местами значения точек a и b
 
 void
 grahams_algorithm(BuildingC *building);
+
+BuildingC*
+merge_buildings(BuildingsVec *buildings_vec); // Функция объединяет здания в массиве и возвращает указатель на здание, которое является объединением всех зданий
+
+BuildingC*
+nc_hull_maker(BuildingsVec *buildings_vec);
 
 void
 freeBuildings(BuildingsVec data);
