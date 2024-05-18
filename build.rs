3b8602dde299bc780e34451e20b804ca3cc54576
puffy_with_eyes <88966131+PuffyWithEyes@users.noreply.github.com>
2024-05-18 16:14:53 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,34 @@
-use std::env;
 use std::path::Path;
+use std::fs;
+
+
+fn run_above_dir(path: &str, c_files: &mut Vec::<String>) {
+    let dir = match fs::read_dir(path) {
+        Ok(dir) => dir,
+        Err(_) => {
+            let format = path.split(".").collect::<Vec<&str>>();
+            let format = match format.get(format.len() - 1) {
+                Some(format) => *format,
+                None => return,
+            };
+
+            if format == "h" || format == "c" {
+                let canon = fs::canonicalize(path).expect("Ошибка! Не удалось канонизировать путь");
+                let canon = canon.to_str().expect("Ошибка! Не удалось перевести полный путь в строку");
+
+                c_files.push(canon.to_string());
+            }
+
+            return;
+        },
+    };
+
+    for doc in dir {
+        if let Ok(doc) = doc {
+            run_above_dir(doc.path().to_str().expect("Ошибка! Не удалось перевести путь в строку"), c_files);
+        }
+    }
+}
 
 
 fn main() {
@@ -15,8 +44,12 @@ fn main() {
 
     println!("cargo:root={}", dst.display());
     println!("cargo:include={}", dst.join("include").display());
-    println!(
-        "cargo:rerun-if-changed={}",
-        env::current_dir().unwrap().to_string_lossy()
-    );
+    
+    let mut c_files = Vec::<String>::new();
+
+    run_above_dir("./", &mut c_files);
+
+    for file in c_files {
+        println!("cargo:rerun-if-changed={file}");
+    }
 }
